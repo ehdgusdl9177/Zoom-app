369b010050acf1a741380e949a9ef25e6ad156c6
ehdgusdl9177 <k23129177@gmail.com>
2023-09-12 11:31:28 +0900

    Recap

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,4 +1,5 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
+
 socket.addEventListener("open", () => {
   console.log("Connected to Server");
 });
--- src/server.js
+++ src/server.js
@@ -15,12 +15,18 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+function onSokcetClose() {
+  console.log("Disconnected from Browser");
+}
+
+function onSokectMessage(message) {
+  console.log(message);
+}
+
 wss.on("connection", (socket) => {
   console.log("Connected to Browser");
-  socket.on("close", () => console.log("Disconnected from Browser"));
-  socket.on("message", (message) => {
-    console.log(message);
-  });
+  socket.on("close", onSokcetClose);
+  socket.on("message", onSokectMessage);
   socket.send("hello!!!");
 });
 
