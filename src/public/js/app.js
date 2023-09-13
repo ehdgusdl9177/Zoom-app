00094c1e88d4b2637509f16bbfc367e5f9a43056
ehdgusdl9177 <k23129177@gmail.com>
2023-09-13 11:50:58 +0900

    room

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,15 +1,25 @@
 const socket = io();
 
 const welcome = document.getElementById("welcome");
-
 const form = welcome.querySelector("form");
+const room = document.getElementById("room");
+
+room.hidden = room;
+
+function showRoom() {
+  welcome.hidden = true;
+  room.hidden = false;
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Room ${roomName}`;
+}
+
+let roomName;
 
 function handleRoomSubmit(event) {
   event.preventDefault();
   const input = form.querySelector("input");
-  socket.emit("enter_room", { payload: input.value }, () => {
-    console.log("Server is done!");
-  });
+  socket.emit("enter_room", input.value, showRoom);
+  roomName = input.value;
   input.value = "";
 }
 
